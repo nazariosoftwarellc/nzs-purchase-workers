225abbc8518592618813eb94df748520eccade1b
Kyle Nazario <knazario@bitovi.com>
2024-01-29 12:06:18 -0500

    Inits worker to check customer purchases

---
+++ init.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS CustomerPurchases;
+CREATE TABLE IF NOT EXISTS CustomerPurchases (
+	  user_email TEXT NOT NULL PRIMARY KEY,
+		app_id TEXT NOT NULL,
+		purchased BOOLEAN NOT NULL,
+		created_at TEXT NOT NULL,
+		updated_at TEXT NOT NULL
+);
+INSERT INTO CustomerPurchases (user_email, app_id, purchased, created_at, updated_at) VALUES ('kylebnazario@gmail.com', 'com.kylenazario.mute-chat', 1, '2024-01-29', '2024-01-29');
