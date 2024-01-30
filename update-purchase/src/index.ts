7163b5918bd1fef40f7e89857cd460e3dc846e9f
Kyle Nazario <knazario@bitovi.com>
2024-01-30 15:08:38 -0500

    Stores purchases on stripe event

--- update-purchase/src/index.ts
+++ update-purchase/src/index.ts
@@ -1,3 +1,4 @@
+import { Stripe } from 'stripe';
 
 export interface Env {
 	DB: D1Database;
@@ -13,6 +14,29 @@ type CustomerPurchase = {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const purchaseEvent: Stripe.PaymentIntentSucceededEvent = await request.json();
+		if (purchaseEvent.type !== 'payment_intent.succeeded') {
+			return new Response('not payment intent succeeded', { status: 200 });
+		}
+		if (!purchaseEvent.data.object.receipt_email) {
+			return new Response('no receipt email', { status: 200 });
+		}
+		const purchaseRecord: CustomerPurchase = {
+			user_email: purchaseEvent.data.object.receipt_email,
+			app_id: purchaseEvent.data.object.metadata.app_id,
+			purchased: true,
+			created_at: new Date().toISOString(),
+			updated_at: new Date().toISOString(),
+		};
+		await env.DB.prepare('INSERT INTO CustomerPurchases (user_email, app_id, purchased, created_at, updated_at) VALUES (?, ?, ?, ?, ?)')
+			.bind(
+				purchaseRecord.user_email,
+				purchaseRecord.app_id,
+				purchaseRecord.purchased,
+				purchaseRecord.created_at,
+				purchaseRecord.updated_at
+			)
+			.run();
+		return new Response('purchase successful', { status: 200 });
 	},
 };
