17505e6244ef76eeb64dd3d4c3795602209ae10d
Kyle Nazario <knazario@bitovi.com>
2024-02-09 11:56:58 -0500

    Verifies events are from stripe

--- update-purchase/src/index.ts
+++ update-purchase/src/index.ts
@@ -3,27 +3,47 @@ import { CustomerPurchase } from '../../shared/types';
 
 export interface Env {
 	DB: D1Database;
+	STRIPE_SECRET_KEY: string;
+	STRIPE_WEBHOOK_SIGNING_SECRET: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const purchaseEvent: Stripe.CheckoutSessionCompletedEvent = await request.json();
-		console.log('purchaseEvent', JSON.stringify(purchaseEvent));
-		const appId = purchaseEvent.data.object.metadata?.app_id;
+		// Verify event came from Stripe
+		const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+		let event: Stripe.CheckoutSessionCompletedEvent;
+		try {
+			event = (await stripe.webhooks.constructEventAsync(
+				await request.text(),
+				request.headers.get('Stripe-Signature') as string,
+				env.STRIPE_WEBHOOK_SIGNING_SECRET
+			)) as Stripe.CheckoutSessionCompletedEvent;
+		} catch (err) {
+			console.error(err);
+			return new Response('invalid signature', { status: 400 });
+		}
+
+		// Log event for debugging
+		console.log('event', event);
+
+		// Verify event has necessary data
+		const appId = event.data.object.metadata?.app_id;
 		if (!appId) {
 			return new Response('no app id', { status: 200 });
 		}
-		if (purchaseEvent.type !== 'checkout.session.completed') {
+		if (event.type !== 'checkout.session.completed') {
 			return new Response('not checkout session succeeded', { status: 200 });
 		}
-		if (purchaseEvent.data.object.payment_status !== 'paid') {
+		if (event.data.object.payment_status !== 'paid') {
 			return new Response('payment not paid', { status: 200 });
 		}
-		if (!purchaseEvent.data.object.customer_details?.email) {
+		if (!event.data.object.customer_details?.email) {
 			return new Response('no customer email', { status: 200 });
 		}
+
+		// Record purchase
 		const purchaseRecord: CustomerPurchase = {
-			user_email: purchaseEvent.data.object.customer_details.email,
+			user_email: event.data.object.customer_details.email,
 			app_id: appId,
 			purchased: 1,
 			created_at: new Date().toISOString(),
@@ -38,6 +58,8 @@ export default {
 				purchaseRecord.updated_at
 			)
 			.run();
+
+		// Return success
 		return new Response('purchase successful', { status: 200 });
 	},
 };
