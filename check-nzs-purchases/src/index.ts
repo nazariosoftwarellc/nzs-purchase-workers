8894487c0a9cca8f44c67076ebb805d85a86bb5c
Kyle Nazario <nazariosoftwarellc@gmail.com>
2024-02-16 10:31:46 -0500

    Handles CORS

--- check-nzs-purchases/src/index.ts
+++ check-nzs-purchases/src/index.ts
@@ -4,6 +4,12 @@ export interface Env {
 	DB: D1Database;
 }
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
+	'Access-Control-Max-Age': '86400'
+};
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { searchParams } = new URL(request.url);
@@ -18,6 +24,6 @@ export default {
 		const customerPurchase = (await env.DB.prepare('SELECT * FROM CustomerPurchases WHERE user_email = ? AND app_id = ?')
 			.bind(email, appId)
 			.first()) as CustomerPurchase | null;
-		return new Response(JSON.stringify(customerPurchase));
+		return new Response(JSON.stringify(customerPurchase), { headers: corsHeaders });
 	}
 };
