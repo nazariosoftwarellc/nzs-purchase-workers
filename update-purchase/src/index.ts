b41876429a82f4adf9723485976652deb17c40ad
Kyle Nazario <knazario@bitovi.com>
2024-02-21 11:31:32 -0500

    Formatting

--- update-purchase/src/index.ts
+++ update-purchase/src/index.ts
@@ -19,57 +19,50 @@ export default {
 };
 
 async function handleStripeWebhook(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// Verify event came from Stripe
-		const stripe = new Stripe(env.STRIPE_SECRET_KEY);
-		let event: Stripe.CheckoutSessionCompletedEvent;
-		try {
-			event = (await stripe.webhooks.constructEventAsync(
-				await request.text(),
-				request.headers.get('Stripe-Signature') as string,
-				env.STRIPE_WEBHOOK_SIGNING_SECRET
-			)) as Stripe.CheckoutSessionCompletedEvent;
-		} catch (err) {
-			console.error(err);
-			return new Response('invalid signature', { status: 400 });
-		}
-
-		// Log event for debugging
-		console.log('event', event);
-
-		// Verify event has necessary data
-		const appId = event.data.object.metadata?.app_id;
-		if (!appId) {
-			return new Response('no app id', { status: 200 });
-		}
-		if (event.type !== 'checkout.session.completed') {
-			return new Response('not checkout session succeeded', { status: 200 });
-		}
-		if (event.data.object.payment_status !== 'paid') {
-			return new Response('payment not paid', { status: 200 });
-		}
-		if (!event.data.object.customer_details?.email) {
-			return new Response('no customer email', { status: 200 });
-		}
-
-		// Record purchase
-		const purchaseRecord: CustomerPurchase = {
-			user_email: event.data.object.customer_details.email,
-			app_id: appId,
-			purchased: 1,
-			created_at: new Date().toISOString(),
-			updated_at: new Date().toISOString()
-		};
-		await env.DB.prepare('INSERT INTO CustomerPurchases (user_email, app_id, purchased, created_at, updated_at) VALUES (?, ?, ?, ?, ?)')
-			.bind(
-				purchaseRecord.user_email,
-				purchaseRecord.app_id,
-				purchaseRecord.purchased,
-				purchaseRecord.created_at,
-				purchaseRecord.updated_at
-			)
-			.run();
-
-		// Return success
-		return new Response('purchase successful', { status: 200 });
+	// Verify event came from Stripe
+	const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+	let event: Stripe.CheckoutSessionCompletedEvent;
+	try {
+		event = (await stripe.webhooks.constructEventAsync(
+			await request.text(),
+			request.headers.get('Stripe-Signature') as string,
+			env.STRIPE_WEBHOOK_SIGNING_SECRET
+		)) as Stripe.CheckoutSessionCompletedEvent;
+	} catch (err) {
+		console.error(err);
+		return new Response('invalid signature', { status: 400 });
 	}
+
+	// Log event for debugging
+	console.log('event', event);
+
+	// Verify event has necessary data
+	const appId = event.data.object.metadata?.app_id;
+	if (!appId) {
+		return new Response('no app id', { status: 200 });
+	}
+	if (event.type !== 'checkout.session.completed') {
+		return new Response('not checkout session succeeded', { status: 200 });
+	}
+	if (event.data.object.payment_status !== 'paid') {
+		return new Response('payment not paid', { status: 200 });
+	}
+	if (!event.data.object.customer_details?.email) {
+		return new Response('no customer email', { status: 200 });
+	}
+
+	// Record purchase
+	const purchaseRecord: CustomerPurchase = {
+		user_email: event.data.object.customer_details.email,
+		app_id: appId,
+		purchased: 1,
+		created_at: new Date().toISOString(),
+		updated_at: new Date().toISOString()
+	};
+	await env.DB.prepare('INSERT INTO CustomerPurchases (user_email, app_id, purchased, created_at, updated_at) VALUES (?, ?, ?, ?, ?)')
+		.bind(purchaseRecord.user_email, purchaseRecord.app_id, purchaseRecord.purchased, purchaseRecord.created_at, purchaseRecord.updated_at)
+		.run();
+
+	// Return success
+	return new Response('purchase successful', { status: 200 });
 }
